ead1bdb0aae54080b13a025367e39ca3767f156a
igorborges182 <igor.silva@ios.org.br>
2023-02-06 11:29:27 -0300

    teste2

--- studio-ios/studio-ios.php
+++ studio-ios/studio-ios.php
@@ -1412,12 +1412,10 @@ function aceita_apenas_numeros( $value, $lead, $field, $form ) {
 
 //function para tirar caracteres especiais
 
-add_filter( 'gform_save_field_value_13_24', 'convertacentos', 10, 4 );
-function convertacentos( $value, $lead, $field, $form ) {
+add_filter( 'gform_save_field_value_13_24', 'consertaespecial', 10, 4 );
+function consertaespecial( $value, $lead, $field, $form ) {
 	GFCommon::log_debug( __METHOD__ . '(): Original value => ' . $value );
-	$value = preg_replace("/[^A-Za-zÀ-ÖØ-öø-ÿ ]/", "", $value );
+	$value = preg_replace("/[A-Za-z0-9- \. \\/]/", "", $value );
 	GFCommon::log_debug( __METHOD__ . '(): Modified value => ' . $value );
 	return $value;
 }
-
-
